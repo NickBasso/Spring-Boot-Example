c9f859a56999784a86922a0083ff89001084f919
Nicolae Basso <nickbassotrue@gmail.com>
2020-12-27 20:33:41 +0200

    init spring boot project

---
+++ spring-boot-project-artifact/src/test/java/study/spring/boot/example/springbootproject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.boot.example.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
