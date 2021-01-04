0772451d6279a6676c3936eef339e571b97a0d34
Nicolae Basso <nickbassotrue@gmail.com>
2021-01-04 04:01:28 +0200

    Add exception handler with response message for getCustomer(int customerId) function

--- spring-boot-project-artifact/src/main/java/study/spring/boot/example/springbootproject/service/CustomerService.java
+++ spring-boot-project-artifact/src/main/java/study/spring/boot/example/springbootproject/service/CustomerService.java
@@ -3,9 +3,11 @@ package study.spring.boot.example.springbootproject.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import study.spring.boot.example.springbootproject.dao.CustomerDAO;
+import study.spring.boot.example.springbootproject.exception.CustomerNotFoundException;
 import study.spring.boot.example.springbootproject.model.Customer;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
@@ -33,13 +35,24 @@ public class CustomerService {
     }
 
     public Customer getCustomer(int customerId) {
-        /*return customerList
+        /*
+            1 - straight forward
+        return customerList
                 .stream()
                 .filter(c -> c.getCustomerId() == customerId)
                 .findFirst()
                 .get();*/
 
-        return customerDAO.findById(customerId).get();
+        /*  2 - using Data Access Object
+           return customerDAO.findById(customerId).get();*/
+
+        //  3 - handles not found exception
+        Optional<Customer> optionalCustomer = customerDAO.findById(customerId);
+
+        if(optionalCustomer.isEmpty())
+            throw new CustomerNotFoundException("Customer with ID " + customerId + " is missing!");
+
+        return optionalCustomer.get();
     }
 
     public Customer updateCustomer(int customerId, Customer customer) {
